18085411c8fff1d1c72947a0c816c4b2ac0d81ea
Yuri Gorokhov <yurigorokhov@gmail.com>
2013-02-14 15:14:41 -0800

    Better grouping layout

--- content.js
+++ content.js
@@ -19,6 +19,8 @@
  * limitations under the License.
  */
 
+_.mixin(_.string.exports());
+
 $(document).ready(function() {
     var statsKey = null;
     var reload = function() {
@@ -41,6 +43,29 @@ $(document).ready(function() {
                     $('#mt-stats-table').find('tr:gt(0)').remove();
                     $('#mt-stats-total').find('*').remove();
                     var content = '';
+                    _(result.stats.requests).each(function(req, idx) {
+                        req.cols = _(req.stats).chain().words(/[;]/).groupBy(function(stat) {
+                            return _(stat).strLeftBack('-').trim();
+                        }).value();
+                    });
+                    var uniqueCols = ['request-time', 'mysql-time', 'mysql'];
+                    _(result.stats.requests).chain().pluck('cols').each(function(col) {
+                        _(col).chain().keys().each(function(key) {
+                            uniqueCols.push(key);
+                        }).value();
+                    }).value();
+                    uniqueCols = _(uniqueCols).uniq();
+                    content += '<tr>' +
+                        '<th class="col1">Verb</th>' +
+                        '<th class="col2">Time</th>' +
+                        '<th class="col3">URL</th>';
+                    _(uniqueCols).each(function(col, idx) {
+                        if(col === 'mysql') {
+                            col = 'mysql (queries)'
+                        }
+                        content += '<th class="col' + (idx + 4) + '">' + col + '</th>';
+                    });
+                    content += '</tr>';
                     chrome.devtools.inspectedWindow.eval(
                         "Deki.BaseHref",
                         function(href, isException) {
@@ -49,10 +74,48 @@ $(document).ready(function() {
                                     '<td class="col1">' + request.verb + '</td>' +
                                     '<td class="col2">' + request.time + '</td>' +
                                     '<td class="col3"><div class="stat-col"><a target="_blank" href="' + href + request.urlPath + '">' + request.urlPath.substring(0, 80) + '</a></div>';
-                                request.stats.split(';').forEach(function(item) {
-                                    content += '<div class="stat-col">' + item + '</div>';
+                                    content += '</td>';
+                                _(uniqueCols).each(function(col, idx) {
+                                    var statMap = { };
+                                    _(request.cols[col]).chain().words(',').each(function(stat) {
+                                        statMap[_(stat).chain().strLeft('=').strRightBack('-').value()] = _(stat).strRight('=');
+                                    });
+                                    var hit = _(statMap['hit']).toNumber(3) || null;
+                                    var miss = _(statMap['miss']).toNumber(3) || null;
+                                    var ratio = _(statMap['ratio']).toNumber(3) || null;
+                                    var reset = _(statMap['reset']).toNumber(3) || null;
+                                    var str = '';
+                                    var tdClass = '';
+                                    if(!miss && !hit && !ratio && !reset) {
+                                        str = _(request.cols[col]).strRightBack('=') || '';
+                                    } else {
+                                        if(!miss && hit && ratio) {
+                                            miss = hit / ratio - hit;
+                                        } else if(!ratio && miss && hit) {
+                                            ratio = hit / (hit + miss);
+                                        }
+                                        if(hit) {
+                                            str += hit || '0';
+                                        }
+                                        if(miss) {
+                                            str += ' / ' + miss;
+                                        }
+                                        if(ratio) {
+                                            if(ratio > 0.99) {
+                                                tdClass = 'good';
+                                            } else if(ratio < 0.5) {
+                                                tdClass = 'bad';
+                                            }
+                                            str += ' ( ' + _(ratio * 100).numberFormat(1) + '% )';
+                                        }
+                                        if(reset) {
+                                            tdClass = 'reset'
+                                            str += '<br />reset: ' + reset;
+                                        } 
+                                    }
+                                    content += '<td class="col' + (idx + 4) + ' ' + tdClass + '">' + str + '</td>';
                                 });
-                                content += '</td></tr>';
+                                content += '</tr>';
                             });
                         $('#mt-stats-table').append(content);
                         }
